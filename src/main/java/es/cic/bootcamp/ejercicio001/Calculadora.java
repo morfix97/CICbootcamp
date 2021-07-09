3da6ead49a597dae1b8762ea2d6704c17f6a5c12
morfix97 <pedromoragolv@gmail.com>
2021-07-09 04:18:08 +0200

    Version inicial de un proyecto con un test unitario

---
+++ src/main/java/es/cic/bootcamp/ejercicio001/Calculadora.java
@@ -0,0 +1,7 @@
+package es.cic.bootcamp.ejercicio001;
+
+public class Calculadora {
+	public int sumar(int s1, int s2) {
+		return s1+s2;
+	}
+}
---
+++ src/test/java/es/cic/bootcamp/ejercicio001/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package es.cic.bootcamp.ejercicio001;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSumar() {
+		Calculadora calculadora = new Calculadora();
+		
+		int resultado = calculadora.sumar(4, 5);
+		
+		assertEquals(9, resultado, "La suma no es correcta");
+	}
+
+}
